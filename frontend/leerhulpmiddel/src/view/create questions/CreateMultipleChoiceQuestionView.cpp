b30785f3652306fbad6444ca9b1260e439908241
Sander Goemans <sandergoemans12@gmail.com>
2024-11-07 22:48:53 +0100

    added MultipleChoiceView

--- frontend/leerhulpmiddel/src/view/create questions/CreateMultipleChoiceQuestionView.cpp
+++ frontend/leerhulpmiddel/src/view/create questions/CreateMultipleChoiceQuestionView.cpp
@@ -1,15 +1,72 @@
 #include "CreateMultipleChoiceQuestionView.h"
-
+#include <QPushButton>
 #include <QLabel>
 #include <QVBoxLayout>
+#include <QIcon>
+#include <QScrollArea>
 
-CreateMultipleChoiceQuestionView::CreateMultipleChoiceQuestionView(QWidget* parent) : QWidget(parent) {
-	QLabel* lblMC = new QLabel(this);
-	lblMC->setText("Multiple Choice");
+CreateMultipleChoiceQuestionView::CreateMultipleChoiceQuestionView(QWidget* parent) : QWidget(parent), m_txtQuestion{ new QTextEdit(this)} {
 
-	QVBoxLayout* mainLayout = new QVBoxLayout(this);
 
-	mainLayout->addWidget(lblMC);
+	QLabel* lblQuestion = new QLabel(this);
+	lblQuestion->setText("Question: ");
 
-	setLayout(mainLayout);
+	QPushButton* btnAddAnswer = new QPushButton("Add an Answer",this);
+
+	connect(btnAddAnswer, &QPushButton::clicked, this, &CreateMultipleChoiceQuestionView::addAnswer);
+
+
+	m_layout = new QVBoxLayout(this);
+
+	
+
+
+	QWidget* contentWidget = new QWidget(this);
+	contentWidget->setLayout(m_layout);
+
+
+	QScrollArea* scrollArea = new QScrollArea(this);
+	scrollArea->setWidget(contentWidget);  // Set the scrollable content widget
+	scrollArea->setWidgetResizable(true);
+	scrollArea->setFrameShape(QFrame::NoFrame);  // No frame around the scroll area
+	scrollArea->setFrameShadow(QFrame::Plain);
+
+	QVBoxLayout* outerLayout = new QVBoxLayout(this);
+	outerLayout->addWidget(lblQuestion);
+	outerLayout->addWidget(m_txtQuestion);
+	outerLayout->addWidget(btnAddAnswer);
+	outerLayout->addWidget(scrollArea);
+	setLayout(outerLayout);
+}
+
+void CreateMultipleChoiceQuestionView::addAnswer() {
+	QHBoxLayout* answerLayout = new QHBoxLayout;
+
+	QTextEdit* txtAnswer = new QTextEdit(this);
+	txtAnswer->setPlaceholderText("Enter an answer");
+	txtAnswer->setFixedHeight(50);
+
+
+	QPushButton *removeButton = new QPushButton(this);
+
+	removeButton->setIcon(QIcon::fromTheme(QIcon::ThemeIcon::ListRemove));
+
+
+	answerLayout->addWidget(txtAnswer);
+	answerLayout->addWidget(removeButton);
+
+
+	m_layout->addLayout(answerLayout);
+
+	connect(removeButton, &QPushButton::clicked, [this, answerLayout]() {
+		m_layout->removeItem(answerLayout);
+		QLayoutItem* child;
+		while ((child = answerLayout->takeAt(0)) != nullptr) {
+			delete child->widget(); // delete the widget
+			delete child;   // delete the layout item
+		}
+		delete answerLayout;
+		});
+
+	update();
 }
--- frontend/leerhulpmiddel/src/view/create questions/CreateMultipleChoiceQuestionView.h
+++ frontend/leerhulpmiddel/src/view/create questions/CreateMultipleChoiceQuestionView.h
@@ -2,11 +2,15 @@
 
 #include <QWidget>
 #include <QTextEdit>
+#include <QBoxLayout>
 
 class CreateMultipleChoiceQuestionView : public QWidget {
 public:
 	CreateMultipleChoiceQuestionView(QWidget* parent = nullptr);
 private:
+	void addAnswer();
 
+	QTextEdit* m_txtQuestion;
+	QBoxLayout* m_layout;
 };
 
