7717c390f9ac15f88e6b6131c633535659ec0587
Sander Goemans <sandergoemans12@gmail.com>
2024-11-30 17:33:52 +0100

    Added network connectivity to server

--- frontend/leerhulpmiddel/src/model/manager/NetworkManager.cpp
+++ frontend/leerhulpmiddel/src/model/manager/NetworkManager.cpp
@@ -1,1 +1,67 @@
 #include "NetworkManager.h"
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QNetworkReply>
+
+NetworkManager::NetworkManager()
+{
+	networkManager = new QNetworkAccessManager(this);
+}
+
+void NetworkManager::login(QString username, QString password)
+{
+	QNetworkRequest request(QUrl("http://localhost:80/user/login"));
+	request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+
+	QJsonObject json = QJsonObject();
+	json["username"] = username;
+	json["password"] = password;
+	
+
+	QByteArray data = QJsonDocument(json).toJson();
+
+	QNetworkReply* reply = networkManager->post(request, data);
+	connect(reply, &QNetworkReply::finished, [this,reply]() {
+		// Check for network errors
+		if (reply->error() != QNetworkReply::NoError) {
+			qDebug() << "Network error:" << reply->errorString();
+			reply->deleteLater();
+			return;
+		}
+
+		QByteArray responseData = reply->readAll();
+		qDebug() << "Response data:" << responseData;
+
+		reply->deleteLater();
+	});
+
+}
+
+void NetworkManager::registerUser(QString username, QString password)
+{
+	QNetworkRequest request(QUrl("http://localhost:80/user/register"));
+	request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+
+	QJsonObject json = QJsonObject();
+	json["username"] = username;
+	json["password"] = password;
+
+	QByteArray data = QJsonDocument(json).toJson();
+
+	QNetworkReply* reply = networkManager->post(request, data);
+	connect(reply, &QNetworkReply::finished, [this, reply]() {
+		// Check for network errors
+		if (reply->error() != QNetworkReply::NoError) {
+			qDebug() << "Network error:" << reply->errorString();
+			QByteArray responseData = reply->readAll();
+			qDebug() << "Response data:" << responseData;
+			reply->deleteLater();
+			return;
+		}
+
+		QByteArray responseData = reply->readAll();
+		qDebug() << "Response data:" << responseData;
+
+		reply->deleteLater();
+		});
+}
--- frontend/leerhulpmiddel/src/model/manager/NetworkManager.h
+++ frontend/leerhulpmiddel/src/model/manager/NetworkManager.h
@@ -1,12 +1,17 @@
 #pragma once
 #include <QString>
 #include <QNetworkAccessManager>
+#include <QWidget>
 
-class NetworkManager
+class NetworkManager : public QObject
 {
-	public:
-	NetworkManager();
+	Q_OBJECT
+public:
+     NetworkManager();
+
 	void login(QString username, QString password);
 	void registerUser(QString username, QString password);
+private:
+	QNetworkAccessManager* networkManager;
 };
 
